4929ba47a2a436435494aeca350da09a791ba3a4
Lukas Jelinek <lukx19@gmail.com>
2025-10-06 22:28:36 +0200

    website: respect user dark mode system preference.

--- docs/website/docusaurus.config.ts
+++ docs/website/docusaurus.config.ts
@@ -87,6 +87,9 @@ const config: Config = {
 	],
 
 	themeConfig: {
+		colorMode: {
+			respectPrefersColorScheme: true
+		},
 		// Replace with your project's social card
 		image: "img/docusaurus-social-card.jpg",
 		navbar: {
